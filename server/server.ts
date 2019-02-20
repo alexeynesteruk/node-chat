4e03195a8863ddbca5deef3a6e7a1e3b1722c4fd
Alexey Nesteruk <anesteruk@exadel.com>
2019-02-20 18:07:33 +0300

    Add socket to project

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,24 @@
 import * as path from "path";
 import express from "express";
+import http from "http";
+import socketIO from "socket.io";
 
 const app = express();
+const server = http.createServer(app); 
+const io = socketIO(server);
+
 const _port = process.env.PORT || 3000;
+
+io.on('connection', (socket)=>{
+    console.log('New user connection');
+
+    socket.on('disconnect', ()=>{
+        console.log('Client disconnected');
+    })
+});
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.listen(_port,()=>{
+server.listen(_port,()=>{
     console.info(`Server is up on port ${_port}`);
 });
